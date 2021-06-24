aed8a87b3ede5e98b45c96f60a2edacbd014ca96
Tuyen <tuyennv@templaza.com>
2021-06-24 15:53:07 +0700

    Update column.php

--- shortcodes/column/tmpl/column.php
+++ shortcodes/column/tmpl/column.php
@@ -3,9 +3,13 @@ defined('TEMPLAZA_FRAMEWORK') or exit();
 
 extract($atts);
 
-//if(!empty($content)){
+if ( ! is_active_sidebar( 'sidebar-main' ) ) {
+    $col_none = 'templaza-sidebar-no-content ';
+}else{
+    $col_none ='';
+}
 ?>
-<div<?php echo isset($tz_id)?' id="'.$tz_id.'"':''; ?> class="<?php
+<div<?php echo isset($tz_id)?' id="'.$tz_id.'"':''; ?> class="<?php echo $col_none;
 echo isset($tz_class)?$tz_class:''?>">
     <?php echo !empty($content)?$content:''; ?></div>
 <?php //} ?>
