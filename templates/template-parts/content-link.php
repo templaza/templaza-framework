447ed85ce67e27be628588460b966a69bcd81199
Tuyen <tuyennv@templaza.com>
2021-06-29 11:09:05 +0700

    Update content-link.php

--- templates/template-parts/content-link.php
+++ templates/template-parts/content-link.php
@@ -1,10 +1,13 @@
 <?php
 defined('ABSPATH') or exit();
+$templaza_link = get_post_meta(get_the_ID(), '_format_link_url', true);
+if($templaza_link){
 ?>
 <div class="templaza-blog-item-link uk-padding-small uk-margin-top uk-margin-bottom">
-    <?php $templaza_link = get_post_meta(get_the_ID(), '_format_link_url', true); ?>
     <a target="_blank" title="<?php the_title(); ?>"
        href="<?php echo esc_url($templaza_link); ?>">
         <?php echo esc_html($templaza_link); ?>
     </a>
 </div>
+<?php
+}
