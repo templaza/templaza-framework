46dd7cd9a39dac8a99d67980329cbbf832d857f3
Sonny Le <sonny@Sonnys-iMac.local>
2021-07-05 11:16:43 +0700

    Update blog meta style

--- templates/template-parts/content-single-meta.php
+++ templates/template-parts/content-single-meta.php
@@ -17,35 +17,35 @@ $show_category      = isset($options[$prefix.'-category'])?filter_var($options[$
 $show_date          = isset($options[$prefix.'-date'])?filter_var($options[$prefix.'-date'], FILTER_VALIDATE_BOOLEAN):true;
 $show_post_view         = isset($options[$prefix.'-post-view'])?filter_var($options[$prefix.'-post-view'], FILTER_VALIDATE_BOOLEAN):true;
 ?>
-<div class="templaza-blog-item-Info templaza-post-meta uk-article-meta">
+<dl class="templaza-blog-item-Info templaza-post-meta uk-article-meta">
     <?php if ($show_date){ ?>
-        <span><i class="fa fa-calendar"></i><?php echo esc_attr(get_the_date()); ?></span>
+        <dd><i class="fa fa-calendar"></i><?php echo esc_attr(get_the_date()); ?></dd>
     <?php } ?>
     <?php if($show_author){ ?>
-        <span class="author">
+        <dd class="author">
             <i class="fas fa-user"></i>
             <?php echo get_the_author_posts_link();?>
-        </span>
+        </dd>
     <?php } ?>
     <?php if ($show_comment_count){ ?>
-        <span class="comment_count">
+        <dd class="comment_count">
             <i class="fas fa-comment"></i>
             <?php do_action('templaza_get_commentcount_post'); ?>
-        </span>
+        </dd>
     <?php } ?>
     <?php if($show_post_view):?>
-        <span class="views">
+        <dd class="views">
             <i class="fas fa-eye"></i>
             <?php do_action('templaza_get_postviews',get_the_ID()); ?>
-        </span>
+        </dd>
     <?php endif; ?>
     <?php if($show_category){ ?>
-        <span class="category">
+        <dd class="category">
             <i class="fas fa-folder"></i>
             <?php the_category(', '); ?>
-        </span>
+        </dd>
     <?php } ?>
     <?php
     edit_post_link();
     ?>
-</div>
+</dl>
