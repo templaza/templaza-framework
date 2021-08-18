56a9e760c86c12321a7b06a730acc230a0868e79
Sonny Le <sonny@Sonnys-iMac.local>
2021-08-18 15:31:58 +0700

    Update single title style

--- templates/template-parts/content-single-title.php
+++ templates/template-parts/content-single-title.php
@@ -1,6 +1,6 @@
 <?php
 defined('ABSPATH') or exit();
 ?>
-<h1 class="templaza-blog-item-title title uk-container-small uk-container uk-text-center">
+<h1 class="templaza-blog-item-title title uk-heading-small uk-container-small uk-container uk-text-center">
     <?php the_title(); ?>
 </h1>
