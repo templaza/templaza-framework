975eb5f56b9b1038cb998d275d3d6338b36ab451
Sonny Le <sonny@Sonnys-iMac.local>
2021-08-27 16:01:42 +0700

    Update Post Navigation

--- templates/template-parts/content-single-next-preview.php
+++ templates/template-parts/content-single-next-preview.php
@@ -10,26 +10,32 @@ if ( $next_post || $prev_post ) {
         $pagination_classes = ' only-one only-next';
     }
     ?>
-    <div class="uk-clearfix templaza-single-other-post uk-margin-large-bottom">
-        <div class="uk-float-left templaza-single-preview-post">
+    <div class="uk-clearfix templaza-post-navigation uk-child-width-1-2 uk-margin-large-bottom uk-grid-collapse" data-uk-grid>
+        <div class="templaza-single-preview-post">
             <?php
             if ( $prev_post ) {
                 ?>
                 <a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>">
-                    <span class="uk-margin-small-right" uk-icon="arrow-left"></span>
-                    <span class="title"><span class="title-inner"><?php echo esc_html_e('Preview Post','templaza-framework'); ?></span></span>
+                    <p class="uk-margin-remove-vertical">
+                        <span class="uk-margin-small-right uk-visible@s" data-uk-icon="arrow-left"></span>
+                        <span class="title"><?php echo esc_html_e('Preview Post','templaza-framework'); ?></span>
+                    </p>
+                    <h5 class="uk-margin-remove-vertical"><?php echo esc_html($prev_post->post_title); ?></h5>
                 </a>
                 <?php
             }
             ?>
         </div>
-        <div class="uk-float-right templaza-single-next-post">
+        <div class="templaza-single-next-post uk-text-right">
             <?php
             if ( $next_post ) {
                 ?>
                 <a class="next-post" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>">
-                    <span class="title"><span class="title-inner"><?php echo esc_html_e('Next Post','templaza-framework'); ?></span></span>
-                    <span class="uk-margin-small-left" uk-icon="arrow-right"></span>
+                    <p class="uk-margin-remove-vertical">
+                        <span class="title"><?php echo esc_html_e('Next Post','templaza-framework'); ?></span>
+                        <span class="uk-margin-small-left uk-visible@s" data-uk-icon="arrow-right"></span>
+                    </p>
+                    <h5 class="uk-margin-medium-left uk-margin-remove-vertical"><?php echo esc_html($next_post->post_title); ?></h5>
                 </a>
                 <?php
             }
