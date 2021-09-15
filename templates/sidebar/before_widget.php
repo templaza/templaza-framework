a979a22badcb9d84e03c5cdd9706ebbf186fc536
Sonny <sonvnn@gmail.com>
2021-09-15 12:02:56 +0700

    Update default style

--- templates/sidebar/before_widget.php
+++ templates/sidebar/before_widget.php
@@ -8,6 +8,6 @@ $options    = array();
 if(class_exists('TemPlazaFramework\Functions')) {
     $options = Functions::get_theme_options();
 }
-$widget_heading_style  = isset($options['widget_box_heading_style'])?$options['widget_box_heading_style']:'style1';
+$widget_heading_style  = isset($options['widget_box_heading_style'])?'-sidebar ' . $options['widget_box_heading_style']:'-sidebar style1';
 ?>
-<div class="widget %2$s <?php echo $widget_heading_style; ?>"><div class="widget-content">
+<div class="widget %2$s<?php echo $widget_heading_style; ?>"><div class="widget-content">
