68d7fae1f8bcdab6be031b7a8513479ed59e2ac8
DuongTV <duongtv@templaza.com>
2021-09-22 09:33:27 +0700

    Fix issue text block widget title html

--- shortcodes/text/tmpl/text.php
+++ shortcodes/text/tmpl/text.php
@@ -8,17 +8,28 @@ use TemPlazaFramework\Templates;
 $options        = Functions::get_theme_options();
 
 if(isset($content) && !empty($content)){
+
+    extract(shortcode_atts(array(
+        'id'                    => uniqid(),
+        'tz_id'                 => '',
+        'tz_css'                => '',
+        'tz_class'              => '',
+        'widget_heading'        => 'h3',
+        'widget_heading_style'  => '',
+    ), $atts));
+
     $type = 'WP_Widget_Text';
     $args = array(
         'before_widget' => '<div class="widget widget-area %s">',
         'after_widget'  => '</div>',
-        'before_title'  => '<h3 class="widgettitle">',
-        'after_title'   => '</h3>',
+        'before_title' => '<'.$widget_heading.' class="widgettitle'.($widget_heading_style?' uk-'.$widget_heading_style:'').'">',
+        'after_title'  => '</'.$widget_heading.'>',
     );
+
     $atts['text']   = $content;
 ?>
-<div<?php echo isset($atts['tz_id'])?' id="'.$atts['tz_id'].'"':''; ?> class="<?php
-echo isset($atts['tz_class'])?trim($atts['tz_class']):''; ?>">
+<div<?php echo !empty($tz_id)?' id="'.esc_attr($tz_id).'"':''; ?> class="<?php
+    echo !empty($tz_class)?esc_attr($tz_class):''; ?>">
     <?php
     the_widget( $type, $atts, $args );
     ?>
