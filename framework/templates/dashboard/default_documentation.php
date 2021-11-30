e60f48fb40ce528e9b1c324c2cf0af6e8a2563a4
Tuyen <tuyennv@templaza.com>
2021-11-30 14:21:49 +0700

    change link doc

--- framework/templates/dashboard/default_documentation.php
+++ framework/templates/dashboard/default_documentation.php
@@ -5,4 +5,4 @@ $theme  = wp_get_theme();
 <span data-uk-icon="icon: file-text; ratio: 3"></span>
 <h3><?php echo __('Documentation', $this -> text_domain); ?></h3>
 <p><?php echo __('Documentation, help files, and video tutorials for beginners and professionals', $this -> text_domain); ?></p>
-<a class="uk-button uk-button-default uk-border-pill uk-margin-top" target="_blank" href="https://document.templaza.com/<?php echo $theme -> get('Name'); ?>">Read more</a>
+<a class="uk-button uk-button-default uk-border-pill uk-margin-top" target="_blank" href="https://document.templaza.com/<?php echo $theme -> get('TextDomain'); ?>">Read more</a>
--- framework/templates/dashboard/default_theme.php
+++ framework/templates/dashboard/default_theme.php
@@ -63,7 +63,7 @@ $theme  = wp_get_theme();
                                     <dt><?php echo __('Forum Support:', $this -> text_domain); ?></dt>
                                     <dd><a class="uk-link-text" href="https://www.templaza.com/forums.html"><?php echo __('Ask a question.', $this -> text_domain); ?></a></dd>
                                     <dt><?php echo __('Online Document:', $this -> text_domain); ?></dt>
-                                    <dd><a class="uk-link-text" href="https://document.templaza.com/<?php echo $theme -> get('Name'); ?>"><?php echo sprintf(__('Check %s Documentation.'), $theme -> get('Name')); ?></a></dd>
+                                    <dd><a class="uk-link-text" href="https://document.templaza.com/<?php echo $theme -> get('TextDomain'); ?>"><?php echo sprintf(__('Check %s Documentation.'), $theme -> get('Name')); ?></a></dd>
                                 </dl>
                             </div>
                         </div>
