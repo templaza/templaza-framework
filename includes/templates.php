320b62ce5d190c4a65bc983a4c968cd844f1cb88
DuongTV <duongtv@templaza.com>
2021-12-22 17:00:34 +0700

    Fix issue devices

--- includes/templates.php
+++ includes/templates.php
@@ -11,16 +11,18 @@ class Templates{
 
     public static $_styles = ['xlarge' => [],'desktop' => [], 'tablet' => [], 'mobile' => []];
     public static $_devices= [
-        'tablet' => '@media (min-width: 640px) and (max-width: 960px)',
-        'laptop' => '@media (min-width: 960px) and (max-width: 1200px)',
-        'desktop' => '',
         'xlarge' => '@media (min-width: 1600px)',
+        'desktop' => '',
+        'laptop' => '@media (min-width: 960px) and (max-width: 1200px)',
+        'tablet' => '@media (min-width: 640px) and (max-width: 960px)',
         'mobile' => '@media (max-width: 640px)',
     ];
 //    public static $_devices= [
+//        'tablet' => '@media (min-width: 640px) and (max-width: 960px)',
+//        'laptop' => '@media (min-width: 960px) and (max-width: 1200px)',
 //        'desktop' => '',
-//        'tablet' => '@media (max-width: 991.98px)',
-//        'mobile' => '@media (max-width: 767.98px)'
+//        'xlarge' => '@media (min-width: 1600px)',
+//        'mobile' => '@media (max-width: 640px)',
 //    ];
 
     protected static $cache = array();
