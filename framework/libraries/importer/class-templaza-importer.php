99a5121df91467a1bb00e23bb95d490d4f3b97a3
DuongTV <duongtv@templaza.com>
2022-04-16 15:55:45 +0700

    Fix issue import menu

--- framework/libraries/importer/class-templaza-importer.php
+++ framework/libraries/importer/class-templaza-importer.php
@@ -40,6 +40,9 @@ if( !class_exists( 'WP_Import')  && file_exists(__DIR__.'/wordpress-importer/cla
 
 if( class_exists( 'WP_Import') ) {
     class TemplazaFramework_Importer extends WP_Import {
+
+        var $processed_parent_menu_items = array();
+
         public function __construct($options = array())
         {
             $options = wp_parse_args( $options, array(
@@ -80,6 +83,26 @@ if( class_exists( 'WP_Import') ) {
                 ${$meta['key']} = $meta['value'];
             }
 
+            if(isset($_menu_item_menu_item_parent) && $_menu_item_menu_item_parent) {
+                $__parent_id    = $this->processed_menu_items[$_menu_item_menu_item_parent];
+
+                $__parent_layout    = get_post_meta($__parent_id, '_templaza_megamenu_layout', true);
+
+                if(!empty($__parent_layout)) {
+
+                    if (!isset($this->processed_parent_menu_items[$__parent_id])) {
+                        $this->processed_parent_menu_items[$__parent_id] = array();
+                    }
+
+                    $index  = 0;
+                    $this->process_menu_layout($__parent_layout, $id, intval($item['post_id']),
+                        $this->processed_parent_menu_items[$__parent_id], $index);
+
+                    update_post_meta($__parent_id, '_templaza_megamenu_layout', $__parent_layout);
+                }
+
+            }
+
             // Update post meta for menu
             if(isset($_templaza_megamenu_layout) && !empty($_templaza_megamenu_layout)){
                 if(is_string($_templaza_megamenu_layout)) {
@@ -93,6 +116,45 @@ if( class_exists( 'WP_Import') ) {
                 }
                 update_post_meta($id, '_templaza_megamenu_settings', $_templaza_megamenu_settings);
             }
+        }
+
+        public function process_menu_layout(&$elements, $new_id, $import_id, &$__parent_layout, &$index = 0){
+
+            foreach($elements as &$element){
+
+                $subitems           = is_array($element) && isset($element['elements']) && !empty($element['elements']);
+
+                $is_megamenu_item   = $element['type'] == 'megamenu_menu_item';
+
+                if($is_megamenu_item){
+                    $is_menu_id     = isset($element['menu_id']) && is_numeric($element['menu_id'])
+                        && (intval($element['menu_id']) == $import_id);
+                    $is_menu_slug   = isset($element['menu_slug']) && is_numeric($element['menu_slug'])
+                        && (intval($element['menu_slug']) == $import_id);
+
+                    $id_index_exists = array_search($new_id, $__parent_layout);
+                    $id_keys        = array_keys($__parent_layout);
+
+                    if(($is_menu_slug || $is_menu_id) && !in_array($new_id, $__parent_layout) && !in_array($index, $id_keys)
+                        && ($id_index_exists == false || ($id_index_exists != false && $id_index_exists != $index))){
+                        if($is_menu_slug) {
+                            $element['menu_slug']       = $new_id;
+                        }elseif($is_menu_id){
+                            $element['menu_id']       = $new_id;
+                        }
+                        $__parent_layout[$index] = $new_id;
+                        return $elements;
+                    }
+                    $index++;
+                }
+
+                if($subitems) {
+                    $this -> process_menu_layout($element['elements'], $new_id, $import_id, $__parent_layout,  $index);
+                }
+
+            }
+
+            return $elements;
         }
     }
 }
