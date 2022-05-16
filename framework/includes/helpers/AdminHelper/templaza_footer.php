79e35769915ba9f41b3e64814774e36254bd9c26
trinhvanduong <duongtv@templaza.com>
2022-05-16 11:31:25 +0700

    Fix issue footer assignment option in templates

--- framework/includes/helpers/AdminHelper/templaza_footer.php
+++ framework/includes/helpers/AdminHelper/templaza_footer.php
@@ -8,6 +8,7 @@ if(!class_exists('TemPlazaFramework\AdminHelper\Templaza_Footer')){
     class Templaza_Footer{
 
         protected static $cache = array();
+        protected static $post_type = 'templaza_footer';
 
         public static function get_items_by_slug(){
             $store_id   = md5(__METHOD__);
@@ -17,13 +18,13 @@ if(!class_exists('TemPlazaFramework\AdminHelper\Templaza_Footer')){
             }
 
             $args     = array(
-                'post_type'      => 'templaza_footer',
+                'post_type'      => static::$post_type,
                 'posts_per_page' => -1,
                 'orderby'        => 'title',
                 'order'          => 'ASC',
                 'meta_query'     => array(
                     array(
-                        'key'   => '_templaza_header__theme',
+                        'key'   => '_'.static::$post_type.'__theme',
                         'value' => get_template()
                     )
                 )
@@ -32,7 +33,6 @@ if(!class_exists('TemPlazaFramework\AdminHelper\Templaza_Footer')){
             $data    = array();
             $tz_posts = \get_posts($args);
             if($tz_posts && count($tz_posts)){
-                //            $data    = array();
                 foreach($tz_posts as $_tz_post){
                     $data[$_tz_post -> post_name] = $_tz_post -> post_title;
                 }
--- framework/includes/helpers/AdminHelper/templaza_style.php
+++ framework/includes/helpers/AdminHelper/templaza_style.php
@@ -8,6 +8,7 @@ if(!class_exists('TemPlazaFramework\AdminHelper\Templaza_Style')){
     class Templaza_Style{
 
         protected static $cache = array();
+        protected static $post_type = 'templaza_style';
 
         public static function get_items_by_slug(){
             $store_id   = md5(__METHOD__);
@@ -17,24 +18,21 @@ if(!class_exists('TemPlazaFramework\AdminHelper\Templaza_Style')){
             }
 
             $args     = array(
-                'post_type'      => 'templaza_style',
+                'post_type'      => static::$post_type,
                 'posts_per_page' => -1,
                 'orderby'        => 'title',
                 'order'          => 'ASC',
                 'meta_query'     => array(
                     array(
-                        'key'   => '_templaza_style_theme',
+                        'key'   => '_'.static::$post_type.'_theme',
                         'value' => get_template()
                     )
                 )
-//                'meta_key'       => '_templaza_style_theme',
-//                'meta_value'     => basename(get_template_directory()),
             );
 
             $data    = array();
             $tz_posts = \get_posts($args);
             if($tz_posts && count($tz_posts)){
-                //            $data    = array();
                 foreach($tz_posts as $_tz_post){
                     $data[$_tz_post -> post_name] = $_tz_post -> post_title;
                 }
