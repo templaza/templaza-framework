58a817e20ac5b0312887b8c6e69acea6c392a965
Tuyen <tuyennv@templaza.com>
2022-12-28 09:55:42 +0700

    - Remove animation a hover - Change document url in Theme dashboard

--- framework/templates/dashboard/default_documentation.php
+++ framework/templates/dashboard/default_documentation.php
@@ -5,4 +5,4 @@ $theme  = wp_get_theme();
 <span data-uk-icon="icon: file-text; ratio: 3"></span>
 <h3><?php echo __('Documentation', 'templaza-framework'); ?></h3>
 <p><?php echo __('Documentation, help files, and video tutorials for beginners and professionals', 'templaza-framework'); ?></p>
-<a class="uk-button uk-button-default uk-border-pill uk-margin-top" target="_blank" href="https://document.templaza.com/<?php echo $theme -> get('Name'); ?>">Read more</a>
+<a class="uk-button uk-button-default uk-border-pill uk-margin-top" target="_blank" href="https://docs.templaza.com/themes/<?php echo strtolower($theme -> get('Name')); ?>">Read more</a>
