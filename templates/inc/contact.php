fbe9947a49ddeb6291f0751fff0815cd62fd3dc0
trinhvanduong <duongtv@templaza.com>
2023-01-04 15:05:56 +0700

    Update register icon html and fix issue login icon for contact info

--- templates/inc/contact.php
+++ templates/inc/contact.php
@@ -9,19 +9,30 @@ $options    = Functions::get_theme_options();
 
 $enable_contact         = isset($options['enable-contact'])?filter_var($options['enable-contact'], FILTER_VALIDATE_BOOLEAN):false;
 if ($enable_contact) {
-    $contact_location         = isset($options['contact-location'])?$options['contact-location']:'';
-    $contact_email            = isset($options['contact-email'])?$options['contact-email']:'';
-    $contact_phone            = isset($options['contact-phone'])?$options['contact-phone']:'';
-    $contact_login            = isset($options['contact-login'])?$options['contact-login']:'';
-    $contact_location_icon    = isset($options['contact-location-icon'])?$options['contact-location-icon']:'';
-    $contact_email_icon       = isset($options['contact-email-icon'])?$options['contact-email-icon']:'';
-    $contact_phone_icon       = isset($options['contact-phone-icon'])?$options['contact-phone-icon']:'';
-    $contact_login_icon       = isset($options['contact-login-icon'])?$options['contact-login-icon']:'';
+    $enable_contact_location  = isset($options['enable-contact-location'])?filter_var($options['enable-contact-location'], FILTER_VALIDATE_BOOLEAN):true;
+    $contact_location         = $enable_contact_location && isset($options['contact-location'])?$options['contact-location']:'';
+    $contact_location_icon    = $enable_contact_location && isset($options['contact-location-icon'])?$options['contact-location-icon']:'';
+
+    $enable_contact_email     = isset($options['enable-contact-email'])?filter_var($options['enable-contact-email'], FILTER_VALIDATE_BOOLEAN):true;
+    $contact_email            = $enable_contact_email && isset($options['contact-email'])?$options['contact-email']:'';
+    $contact_email_icon       = $enable_contact_email && isset($options['contact-email-icon'])?$options['contact-email-icon']:'';
+
+    $enable_contact_phone     = isset($options['enable-contact-phone'])?filter_var($options['enable-contact-phone'], FILTER_VALIDATE_BOOLEAN):true;
+    $contact_phone            = $enable_contact_phone && isset($options['contact-phone'])?$options['contact-phone']:'';
+    $contact_phone_icon       = $enable_contact_phone && isset($options['contact-phone-icon'])?$options['contact-phone-icon']:'';
+
+    $enable_contact_login     = isset($options['enable-contact-login'])?filter_var($options['enable-contact-login'], FILTER_VALIDATE_BOOLEAN):true;
+    $contact_login            = $enable_contact_login && isset($options['contact-login'])?$options['contact-login']:'';
+    $contact_login_icon       = $enable_contact_login && isset($options['contact-login-icon'])?$options['contact-login-icon']:'';
+
+    $enable_contact_register  = isset($options['enable-contact-register'])?filter_var($options['enable-contact-register'], FILTER_VALIDATE_BOOLEAN):true;
+    $contact_register         = $enable_contact_login && isset($options['contact-register'])?$options['contact-register']:'';
+    $contact_register_icon    = $enable_contact_login && isset($options['contact-register-icon'])?$options['contact-register-icon']:'';
 
 
 }
 
-if(!empty($contact_location) || !empty($contact_email)  || !empty($contact_phone ) || !empty($contact_phone)){
+if(!empty($contact_location) || !empty($contact_email)  || !empty($contact_phone ) || !empty($contact_login)){
 ?>
 <div class="tz-header-contact">
     <?php
@@ -61,6 +72,16 @@ if(!empty($contact_location) || !empty($contact_email)  || !empty($contact_phone
             <i class="contact-icon <?php echo esc_attr($contact_login_icon);?>"></i>
         <?php } ?>
         <a href="<?php echo esc_url(admin_url());?>"><?php echo esc_attr($contact_login);?></a>
+        </span>
+        <?php
+    }
+    if(!empty($contact_register)){
+        ?>
+        <span class="contact-register">
+        <?php if($contact_register_icon){ ?>
+            <i class="contact-icon <?php echo esc_attr($contact_register_icon);?>"></i>
+        <?php } ?>
+        <a href="<?php echo esc_url(admin_url());?>"><?php echo esc_attr($contact_register);?></a>
         </span>
         <?php
     }
