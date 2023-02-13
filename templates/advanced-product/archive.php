b0bad2e77b8859290bb39279541cb29fd90c3948
trinhvanduong <duongtv@templaza.com>
2023-02-13 16:43:30 +0700

    Update sort by html for advanced-product archive

--- templates/advanced-product/archive.php
+++ templates/advanced-product/archive.php
@@ -37,9 +37,24 @@ if($ap_layout == 'masonry'){
 <?php
 if ( have_posts()) {
     ?>
-<div class="templaza-ap-archive-view uk-flex uk-flex-right uk-text-right" data-uk-switcher data-ap-archive-view="<?php echo $grid_view;?>">
-    <span class="grid<?php echo $grid_view == 'grid'?' uk-active':'';?>" data-uk-icon="grid" data-ap-archive-view-item="grid"></span>
-    <span class="list<?php echo $grid_view == 'list'?' uk-active':'';?>" data-uk-icon="list" data-ap-archive-view-item="list"></span>
+    <div class="uk-flex uk-flex-middle uk-flex-right">
+    <div class="templaza-ap-archive-sort uk-flex uk-flex-middle">
+        <label class="uk-width-auto"><?php echo esc_html__('Sort By', 'templaza-framework')?></label>
+        <div class="uk-form-controls">
+            <select>
+                <option value="date_high"><?php echo esc_html__('Date: Newest First', 'templaza-framework')?></option>
+                <option value="date_low"><?php echo esc_html__('Date: Oldest First', 'templaza-framework')?></option>
+                <option value="title_low"><?php echo esc_html__('Title: A - Z', 'templaza-framework')?></option>
+                <option value="title_high"><?php echo esc_html__('Title: Z - A', 'templaza-framework')?></option>
+                <option value="price_high"><?php echo esc_html__('Price: High To Low', 'templaza-framework')?></option>
+                <option value="price_low"><?php echo esc_html__('Price: Low To High', 'templaza-framework')?></option>
+            </select>
+        </div>
+    </div>
+    <div class="templaza-ap-archive-view uk-flex uk-flex-right uk-text-right" data-uk-switcher data-ap-archive-view="<?php echo $grid_view;?>">
+        <span class="grid<?php echo $grid_view == 'grid'?' uk-active':'';?>" data-uk-icon="grid" data-ap-archive-view-item="grid"></span>
+        <span class="list<?php echo $grid_view == 'list'?' uk-active':'';?>" data-uk-icon="list" data-ap-archive-view-item="list"></span>
+    </div>
 </div>
 <div class="templaza-ap-archive templaza-ap-grid
   uk-child-width-1-<?php echo esc_attr($ap_col);?>@l
