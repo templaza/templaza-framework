4f3d2b80bf0e056ad3844b7e84263453cc5babbd
Tuyen <tuyennv@templaza.com>
2023-04-06 09:26:47 +0700

    - Add limit height in search options

--- gutenberg-blocks/advanced-products-filter/build/index.asset.php
+++ gutenberg-blocks/advanced-products-filter/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '57d9cbfa4024e0bbca32');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '28013c4e6c2a92db3ab7');
