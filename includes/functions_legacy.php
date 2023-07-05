5b046cc067f3056651145cc4709d1179e000ae68
trinhvanduong <duongtv@templaza.com>
2023-07-05 14:21:58 +0700

    Fix issue function_exists of ReduxFramework

--- includes/functions_legacy.php
+++ includes/functions_legacy.php
@@ -3,7 +3,12 @@
 defined( 'ABSPATH' ) || exit;
 
 use TemPlazaFramework\Functions;
+use TemPlazaFramework\AdminHelper\Templaza_Style;
 
 function templaza_framework_get_templaza_style_by_slug(){
     return Functions::get_templaza_style_by_slug();
+}
+
+function templaza_framework_get_templaza_style_items_by_slug(){
+    return Templaza_Style::get_items_by_slug();
 }
