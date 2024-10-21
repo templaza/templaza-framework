863215f3396602350e5920b00fff4048110b33cd
Tuyen <tuyennv@templaza.com>
2024-10-21 14:46:30 +0700

    add option image module templaza framework

--- shortcodes/uiimage/tmpl/uiimage.php
+++ shortcodes/uiimage/tmpl/uiimage.php
@@ -4,16 +4,39 @@ defined('TEMPLAZA_FRAMEWORK') or exit();
 
 use TemPlazaFramework\Functions;
 use TemPlazaFramework\Templates;
+use TemPlazaFramework\CSS;
 extract(shortcode_atts(array(
 	'id'                    => uniqid(),
 	'tz_id'                 => '',
 	'tz_css'                => '',
 	'tz_class'              => '',
-	'image'              => '',
+	'image'                 => '',
+	'image_url'             => '',
+	'image_radius'          => '',
+	'image_custom_height'   => '',
+	'image_height'          => '',
+	'image_transition'      => '',
 ), $atts));
 if(!empty($image)){
 	$image      =   json_decode($image);
-	if (isset($image->url) && $image->url) {
+	$image_large = wp_get_attachment_image_url($image->id,'large');
+    $cover = $toggle = $transition_opaque = $ripple_html = $ripple_cl ='';
+	if($image_custom_height){
+	    $cover = 'data-uk-cover';
+    }
+	if($image_transition){
+	    $toggle = 'uk-transition-toggle';
+	    $transition_opaque = ' uk-transition-opaque';
+    }
+    if($image_transition =='ripple'){
+        $ripple_html = '<div class="templaza-ripple-circles uk-position-center uk-transition-fade">
+                        <div class="circle1"></div>
+                        <div class="circle2"></div>
+                        <div class="circle3"></div>
+                    </div>';
+        $ripple_cl = ' templaza-thumb-ripple ';
+    }
+	if (isset($image_large) && $image_large) {
 		// String manipulation should be faster than pathinfo() on newer PHP versions.
 		$dot = strrpos($image->url, '.');
 
@@ -25,16 +48,21 @@ if(!empty($image)){
 			if (strpos($ext, '/') === false && strpos($ext, '\\') === false)
 			{
 				$svg    =    $ext == 'svg' ? 'uk-svg' : '';
-				echo '<div id="'.esc_attr($tz_id).'" class="'.esc_attr($tz_class).'">';
+				echo '<div id="'.esc_attr($tz_id).'" class="'.esc_attr($tz_class).'"><div class="tz-image-el uk-cover-container '.$toggle.$ripple_cl.'" >';
+				if(isset($image_url) && $image_url !=''){
+                echo '<a class="tz-img  uk-display-block" target="_blank" href="'.esc_url($image_url).'">';
+                }
 				if ($svg) {
-					echo '<img class="uk-preserve" data-uk-img="data-src:'.esc_url($image->url).'" src="'.esc_url($image->url).'" alt="'.esc_attr('Logo').'" data-uk-svg>';
+					echo '<img class="uk-preserve '.$image_transition.' " data-uk-img="data-src:'.esc_url($image_large).'" src="'.esc_url($image_large).'" alt="'.esc_attr('Logo').'" data-uk-svg>';
 				} else {
-					echo '<img data-src="'.esc_url($image->url).'" src="'.esc_url($image->url).'" alt="'.esc_attr('Logo').'" data-uk-img>';
+					echo '<img class="'.$image_transition.$transition_opaque.'" '.$cover.' data-src="'.esc_url($image_large).'" src="'.esc_url($image_large).'" alt="'.esc_attr('Logo').'" data-uk-img>';
 				}
-				echo '</div>';
+                echo wp_kses($ripple_html, 'post');
+				if(isset($image_url)){
+                echo '</a>';
+                }
+                echo '</div></div>';
 			}
 		}
-	} elseif (file_exists(get_template_directory().'/assets/images/logo.svg')) {
-		echo '<img data-uk-img="data-src:'.esc_url(get_template_directory_uri().'/assets/images/logo.svg').'" src="'.esc_url(get_template_directory_uri().'/assets/images/logo.svg').'" alt="'.esc_attr('Logo').'" data-uk-svg>';
 	}
 }
--- shortcodes/uiimage/uiimage.php
+++ shortcodes/uiimage/uiimage.php
@@ -3,6 +3,8 @@
 defined('TEMPLAZA_FRAMEWORK') or exit();
 
 use TemPlazaFramework\Functions;
+use TemPlazaFramework\Templates;
+use TemPlazaFramework\CSS;
 
 if(!class_exists('TemplazaFramework_ShortCode_UIImage')){
 	class TemplazaFramework_ShortCode_UIImage extends TemplazaFramework_ShortCode {
@@ -25,9 +27,90 @@ if(!class_exists('TemplazaFramework_ShortCode_UIImage')){
 						'desc'     => __( 'Basic media uploader with disabled URL input field.', 'templaza-framework' ),
 						'subtitle' => __( 'Select an image', 'templaza-framework' )
 					),
+                    array(
+                        'id'       => 'image_url',
+                        'type'     => 'text',
+                        'title'    => esc_html__('Image Url', 'templaza-framework'),
+                    ),
+                    array(
+                        'id'       => 'image_radius',
+                        'type'     => 'spacing',
+                        'mode'     => 'border-radius',
+                        'allow_responsive'    => true,
+                        'units'  => array('px', '%' ),
+                        'title'    => __('Image Border Radius', 'templaza-framework'),
+                        'default'  => '',
+                    ),
+                    array(
+                        'id'       => 'image_custom_height',
+                        'type'     => 'switch',
+                        'title'    => __( 'Image Custom Height', 'templaza-framework' ),
+                        'default'  => true,
+                    ),
+
+                    array(
+                        'id'       => 'image_height',
+                        'type'     => 'spinner',
+                        'title'    => __('Image Custom Height', 'templaza-framework'),
+                        'default'  => '300',
+                        'min'      => '0',
+                        'step'     => '1',
+                        'max'      => '1000',
+                        'required' => array('image_custom_height', '=' , true)
+                    ),
+                    array(
+                        'id'       => 'image_transition',
+                        'type'     => 'select',
+                        'title'    => esc_html__('Select Option', 'templaza-framework'),
+                        'options'  => array(
+                            '' => __('None', 'templaza-framework'),
+                            'uk-transition-scale-up' => __('Scales Up', 'templaza-framework'),
+                            'uk-transition-scale-down' => __('Scales Down', 'templaza-framework'),
+                            'ripple' => __('Ripple', 'templaza-framework'),
+                        ),
+                        'default'  => '',
+                    ),
+
 				)
 			);
 		}
+
+        public function prepare_params($params, $element, $parent_el){
+            $css = Templates::$_devices;
+
+            $params = parent::prepare_params($params, $element, $parent_el);
+            $image_styles = [];
+            $custom_css_name    = 'tz_custom_'.$element['id'];
+            if (isset($params['image_custom_height'])) {
+                if (!empty($params['image_height'])) {
+                    $image_styles[] = '.'. $custom_css_name . ' .tz-image-el{ height: ' . $params['image_height'] . 'px !important;}';
+                }
+            }
+
+            Templates::add_inline_style(implode('', $image_styles));
+
+
+            if(isset($params['image_radius']) && !empty($params['image_radius'])){
+
+                $radius    = CSS::make_spacing_redux('border-radius', $params['image_radius'], true, 'px');
+
+                if(!empty($radius)){
+                    if(is_array($radius)){
+                        foreach($css as $device => $pcss){
+                            if(!empty($radius[$device])) {
+                                $style =  '.' . $custom_css_name . ' .tz-image-el{' . $radius[$device] . '}';
+                                Templates::add_inline_style($style, $device);
+                            }
+                        }
+                    }
+                    else{
+                        Templates::add_inline_style('.' . $custom_css_name . ' .tz-image-el{' . $radius . '}');
+                    }
+                }
+            }
+
+            return $params;
+        }
 	}
 
 }
